b1fbb4400d7e204627a97d4959419654245c569e
Artem Hryshko <artemggg2222@gmail.com>
2024-03-26 21:14:00 +0200

    add url, serializer and view for ShowTheme model

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,4 +1,12 @@
-urlpatterns = [
-]
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import ShowThemeViewSet
+
+router = routers.DefaultRouter()
+
+router.register("show_themes", ShowThemeViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "planetarium"
